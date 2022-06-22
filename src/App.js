b484d8f1b2c51fef66930ce0905f110bb3bb21bb
sreyom31 <sreyom31@gmail.com>
2022-06-22 23:25:02 +0530

    file structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import Home from './pages/home';
-import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Pool from './pages/pool';
+import Staking from './pages/staking';
+import Claim from './pages/claim';
+import Apply from './pages/apply';
 import Header from './shared/components/header';
+
+import './App.scss';
+import Footer from './shared/components/footer';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <div className="pt-[10vh] pb-[10vh]">
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Header />
+        <div className="pt-[10vh] pb-[10vh]">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/pool" element={<Pool />} />
+            <Route path="/staking" element={<Staking />} />
+            <Route path="/claim" element={<Claim />} />
+            <Route path="/apply" element={<Apply />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+
+      <Footer />
+    </>
   );
 };
 
