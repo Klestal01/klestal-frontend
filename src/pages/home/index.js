be941cd2dce33c6cc2e402b9d3def9a2e71cd26d
sreyom31 <sreyom31@gmail.com>
2022-06-22 23:33:35 +0530

    hero component home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,9 @@
+import Hero from './hero';
 const Home = () => {
   return (
-    <>
-      <div className=" bg-bgColor">
-        <h1>hello</h1>
-      </div>
-    </>
+    <div className="w-full ">
+      <Hero />
+    </div>
   );
 };
 
