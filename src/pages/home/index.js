640ceb96474c1b8154b0596ea87fb4dd03da9e93
sreyom31 <sreyom31@gmail.com>
2022-06-22 23:38:04 +0530

    upcoming pool home component

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,10 @@
 import Hero from './hero';
+import UpcommingPool from './upcoming-pool';
 const Home = () => {
   return (
     <div className="w-full ">
       <Hero />
+      <UpcommingPool />
     </div>
   );
 };
