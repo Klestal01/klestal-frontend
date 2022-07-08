826ba7c9f9a53f61aed3019e550be4bf2726c1c7
sreyom31 <sreyom31@gmail.com>
2022-07-08 10:39:59 +0530

    pool details page

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
     <>
       <BrowserRouter>
         <Header />
-        <div className="pt-[10vh] pb-[10vh]">
+        <div className="pt-[10vh] pb-[10vh] text-white">
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route path="/pool" element={<Pool />} />
