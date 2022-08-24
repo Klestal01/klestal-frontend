16f880cb00703a6f46a8768f5dd704fdd1bfff29
sreyom31 <sreyom31@gmail.com>
2022-08-24 19:08:42 +0530

    home hero fixed
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- src/pages/home/hero.js
+++ src/pages/home/hero.js
@@ -7,15 +7,15 @@ const Hero = () => {
         alt="background"
         draggable={false}
       />
-      <div className="z-1 relative md:absolute md:top-[20vh] px-[8vw]  lg:pl-[12vw] w-full text-center lg:text-left lg:w-[55vw] text-white">
-        <h1 className="text-4xl sm:text-5xl md:text-3xl lg:text-2xl xl:text-6xl  sm:px-0 font-sans font-bold -top-20 left-0 sm:top-20 sm:static absolute">
+      <div className="z-1 relative md:absolute md:top-[20vh] px-[8vw] lg:pl-[12vw] w-full text-center lg:text-left lg:w-[55vw] text-white">
+        <h1 className="text-4xl sm:text-5xl md:text-3xl lg:text-2xl xl:text-6xl  sm:px-0 font-sans font-bold -top-60 left-0 sm:top-20 sm:static absolute">
           The Foremost interactive Metaverse Launchpad
         </h1>
-        <p className="opacity-50 sm:mt-10 md:mt-5 pt-[15vh] sm:pt-0">
+        <p className="text-lg opacity-50 sm:mt-10 md:mt-5 pt-8 sm:pt-0">
           Klestal is an IDO compatible, fully decentralized and interactive
           platform for cherry-picked projects in the metaverse.
         </p>
-        <div className="flex md:flex-row flex-col lg:gap-5 gap-2 mt-3 text-md xl:text-xl justify-center items-center lg:justify-start ">
+        <div className="flex md:flex-row flex-col lg:gap-5 gap-2 my-8 sm:mt-3 text-xl justify-center items-center lg:justify-start ">
           <button
             type="button"
             className="bg-gradient-to-r from-orange to-red rounded-3xl px-10 py-2 w-fit"
